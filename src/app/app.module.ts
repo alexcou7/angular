540fed85726bc19e76eaec0b83f839d33a8ba804
alexcou7 <alex11couso@gmail.com>
2019-06-18 10:52:29 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, AngularFontAwesomeModule, NoopAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    AngularFontAwesomeModule,
+    NoopAnimationsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBzEFONC0Vk7yNoPngPEdLcSgr2aUqS2WQ'
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
